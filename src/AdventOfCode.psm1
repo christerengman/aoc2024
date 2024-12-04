fa71b106125a7a201b00657dcd1d9fcdc5855283
christerengman <github@christer.se>
2024-12-04 22:01:54 +0100

    day 4.1

--- src/AdventOfCode.psm1
+++ src/AdventOfCode.psm1
@@ -1,86 +1,113 @@
 function Get-Input {
-    param (
-        [int]$Day
-    )
+  param (
+    [int]$Day
+  )
 
-    $path = Join-Path $PSScriptRoot ("../input/{0:d2}.txt" -f $Day)
+  $path = Join-Path $PSScriptRoot ("../input/{0:d2}.txt" -f $Day)
 
-    return Get-Content $path
+  return Get-Content $path
 }
 
 function Get-Answer01 {
-    [int[]] $a = @()
-    [int[]] $b = @()
+  [int[]] $a = @()
+  [int[]] $b = @()
 
-    Get-Input -Day 1 | ForEach-Object {
-        $x, $y = $_ -split ' +'
-        $a += $x
-        $b += $y
-    }
+  Get-Input -Day 1 | ForEach-Object {
+    $x, $y = $_ -split ' +'
+    $a += $x
+    $b += $y
+  }
 
-    $a = $a | Sort-Object
-    $b = $b | Sort-Object
+  $a = $a | Sort-Object
+  $b = $b | Sort-Object
 
-    $d = 0
-    $s = 0
-    for ($i = 0; $i -lt $a.Count; $i++) {
-        $x = $a[$i]
-        $d += [math]::Abs($x - $b[$i])
-        $s += $x * [array]::FindAll($b, [Predicate[int]] { $args[0] -eq $x }).Count
-    }
+  $d = 0
+  $s = 0
+  for ($i = 0; $i -lt $a.Count; $i++) {
+    $x = $a[$i]
+    $d += [math]::Abs($x - $b[$i])
+    $s += $x * [array]::FindAll($b, [Predicate[int]] { $args[0] -eq $x }).Count
+  }
 
-    return $d, $s
+  return $d, $s
 }
 
 function Get-Answer02 {
-    $a1 = $a2 = 0
-    Get-Input -Day 2 |
-    ForEach-Object {
-        [System.Collections.ArrayList]$y = $_ -split ' '
+  $a1 = $a2 = 0
+  Get-Input -Day 2 |
+  ForEach-Object {
+    [System.Collections.ArrayList]$y = $_ -split ' '
 
-        :outer for ($j = -1; $j -lt $y.Count; $j++) {
-            $x = $y.Clone()
-            if ($j -ge 0) {
-                $x.RemoveAt($j)
-            }
-            $prevDiff = 0
-            for ($i = 1; $i -lt $x.Count; $i++) {
-                $diff = $x[$i] - $x[$i - 1]
-                if ($prevDiff * $diff -lt 0 -or $diff -eq 0 -or [Math]::Abs($diff) -gt 3 ) {
-                    continue outer
-                }
-                $prevDiff = $diff
-            }
-            if ($j -eq -1) {
-                $a1++
-            }
-            $a2++
-            break
+    :outer for ($j = -1; $j -lt $y.Count; $j++) {
+      $x = $y.Clone()
+      if ($j -ge 0) {
+        $x.RemoveAt($j)
+      }
+      $prevDiff = 0
+      for ($i = 1; $i -lt $x.Count; $i++) {
+        $diff = $x[$i] - $x[$i - 1]
+        if ($prevDiff * $diff -lt 0 -or $diff -eq 0 -or [Math]::Abs($diff) -gt 3 ) {
+          continue outer
         }
+        $prevDiff = $diff
+      }
+      if ($j -eq -1) {
+        $a1++
+      }
+      $a2++
+      break
     }
+  }
 
-    return $a1, $a2
+  return $a1, $a2
 }
 
 function Get-Answer03 {
-    $a1 = 0
-    $re1 = [regex]::new("mul\(([0-9]+),([0-9]+)\)")
+  $a1 = 0
+  $re1 = [regex]::new("mul\(([0-9]+),([0-9]+)\)")
 
-    $s = (Get-Input -Day 3) -join ""
-    $re1.Matches($s) | ForEach-Object {
-        $x = [int]$_.Groups[1].Value
-        $y = [int]$_.Groups[2].Value
-        $a1 += $x * $y
-    }
+  $s = (Get-Input -Day 3) -join ""
+  $re1.Matches($s) | ForEach-Object {
+    $x = [int]$_.Groups[1].Value
+    $y = [int]$_.Groups[2].Value
+    $a1 += $x * $y
+  }
 
-    $a2 = 0
-    $re2 = [regex]::new("don't\(\).*?(do\(\)|$)")
-    $s2 = $re2.Replace($s, "")
-    $re1.Matches($s2) | ForEach-Object {
-        $x = [int]$_.Groups[1].Value
-        $y = [int]$_.Groups[2].Value
-        $a2 += $x * $y
-    }
+  $a2 = 0
+  $re2 = [regex]::new("don't\(\).*?(do\(\)|$)")
+  $s2 = $re2.Replace($s, "")
+  $re1.Matches($s2) | ForEach-Object {
+    $x = [int]$_.Groups[1].Value
+    $y = [int]$_.Groups[2].Value
+    $a2 += $x * $y
+  }
 
-    return $a1, $a2
+  return $a1, $a2
+}
+
+function Get-Answer04 {
+  $a1 = 0
+
+  $i = Get-Input -Day 4
+  $sep = "[\w,]"
+  $south = $i[0].Length
+  $southeast = $south + 1
+  $southwest = $south - 1
+  $res = @(
+    "XMAS" # ➡️
+    "SAMX" # ⬅️
+    "(?=X$sep{$south}M$sep{$south}A$sep{$south}S)" # ⬇️
+    "(?=S$sep{$south}A$sep{$south}M$sep{$south}X)" # ⬆️
+    "(?=X$sep{$southeast}M$sep{$southeast}A$sep{$southeast}S)" # ↘️
+    "(?=S$sep{$southeast}A$sep{$southeast}M$sep{$southeast}X)" # ↖️
+    "(?=X$sep{$southwest}M$sep{$southwest}A$sep{$southwest}S)" # ↙️
+    "(?=S$sep{$southwest}A$sep{$southwest}M$sep{$southwest}X)" # ↗️
+  )
+
+  $s = $i -join ","
+  foreach ($re in $res) {
+    $a1 += [regex]::Matches($s, $re).Count
+  }
+
+  return $a1
 }
--- test/AdventOfCode.Tests.ps1
+++ test/AdventOfCode.Tests.ps1
@@ -1,13 +1,13 @@
 BeforeAll {
-  $Script:ModuleName = "AdventOfCode"
-  Import-Module $PSScriptRoot/../src/$Script:ModuleName.psm1 -Force
+  $ModuleName = "AdventOfCode"
+  Import-Module $PSScriptRoot/../src/$ModuleName.psm1 -Force
 }
 
 Describe "AdventOfCode" {
 
   Context "Day 1" {
     It "Should be correct" {
-      InModuleScope $Script:ModuleName {
+      InModuleScope $ModuleName {
         Mock Get-Input { @'
 3   4
 4   3
@@ -15,7 +15,7 @@ Describe "AdventOfCode" {
 1   3
 3   9
 3   3
-'@ -split "`n" }
+'@ -split [Environment]::NewLine }
 
         $a, $b = Get-Answer01
         $a | Should -Be 11
@@ -26,7 +26,7 @@ Describe "AdventOfCode" {
 
   Context "Day 2" {
     It "Should be correct" {
-      InModuleScope $Script:ModuleName {
+      InModuleScope $ModuleName {
         Mock Get-Input { @'
 7 6 4 2 1
 1 2 7 8 9
@@ -34,7 +34,7 @@ Describe "AdventOfCode" {
 1 3 2 4 5
 8 6 4 4 1
 1 3 6 7 9
-'@ -split "`n" }
+'@ -split [Environment]::NewLine }
 
         $a, $b = Get-Answer02
         $a | Should -Be 2
@@ -45,21 +45,43 @@ Describe "AdventOfCode" {
 
   Context "Day 3" {
     It "Should be correct" {
-      InModuleScope $Script:ModuleName {
+      InModuleScope $ModuleName {
         Mock Get-Input { @'
 xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
-'@ -split "`n" }
+'@ }
 
         $a, $b = Get-Answer03
         $a | Should -Be 161
 
         Mock Get-Input { @'
 xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
-'@ -split "`n" }
+'@ }
 
         $a, $b = Get-Answer03
         $b | Should -Be 48
       }
+    }
+  }
+}
+
+Context "Day 4" {
+  It "Should be correct" {
+    InModuleScope $ModuleName {
+      Mock Get-Input { @'
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+'@ -split [Environment]::NewLine }
+
+      $a = Get-Answer04
+      $a | Should -Be 18
     }
   }
 }
